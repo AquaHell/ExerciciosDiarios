bd15328c4a7e8fbb50060085f53160aae4d55452
UserPL022Pc11 <UserPL022Pc11@SLFRPL022PC11.ensino.atec.local>
2016-10-10 08:16:55 +0100

    First

--- src/exercicios071016/Treino.java
+++ src/exercicios071016/Treino.java
@@ -1,11 +1,13 @@
 package exercicios071016;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Treino {
 	public Treino() {
 		exercicio1();
 		exercicio2();
+		exercicio3();
 	}
 
 	private void exercicio1() {
@@ -30,15 +32,15 @@ public class Treino {
 		System.out.println("Introduza o Terceiro Numero");
 		int num3 = (new Scanner(System.in)).nextInt();
 
-		if (num1 > num2) {
-			if (num1 > num3) {
+		if (num1 >= num2) {
+			if (num1 >= num3) {
 				System.out.println("O Primeiro numero eh o maior");
 			} else {
 				System.out.println("O Terceiro numero eh o maior");
 			}
 		}
-		if (num2 > num1) {
-			if (num2 > num3) {
+		if (num2 >= num1) {
+			if (num2 >= num3) {
 				System.out.println("O Segundo numero eh o maior");
 			} else {
 				System.out.println("O Terceiro numero eh o maior");
@@ -47,4 +49,21 @@ public class Treino {
 
 	}
 
+	private void exercicio3() {
+		int limite = 5;
+		int rand = (new Random()).nextInt(limite);
+		int num = 0;
+
+		for (int i = 1; i <= limite; i++) {
+			System.out.println("Escolha um numero entre 1 a 5");
+			num = (new Scanner(System.in).nextInt());
+			if (num == rand) {
+				System.out.println("Acertou no numero na " + i + " Tentativa");
+				break;
+			} else {
+				System.out.println("Falhou \nTry Again \nAinda tem "+(limite-i)+" Tentativas");
+			}
+		}
+
+	}
 }
